4a7999bd436947afb3719e26ddc40fdffa68ac8b
Brenno Alves <brennoao@outlook.com>
2023-04-04 12:25:11 -0300

    Header Criado

---
+++ saiba-politico/components/Header/Header.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Image from 'next/image'
+import Logo from '../../public/Logo.svg'
+
+import Style from './Header.module.css'
+
+import { IoSearch } from "react-icons/io5";
+
+const Header = () => {
+  return (
+    <>
+      <header className={Style.Div1_Principal}>
+          <Image src={Logo} alt="Logo_Saiba-Politico" className={Style.Image}/>
+
+          <section className={Style.Div1_Section}>
+              <input type={'search'} placeholder="Pesquise o deputado aqui" />
+              <div className={Style.Div1_Section_Icon}>
+                <IoSearch />
+              </div>
+          </section>
+      </header>
+    </>
+  )
+}
+
+export default Header
