99a2a22a04a39a412c83bc74196f404d17c7da99
Brenno Alves <brennoao@outlook.com>
2023-04-05 19:46:44 -0300

    Footer-Final

--- saiba-politico/components/Footer/Footer.js
+++ saiba-politico/components/Footer/Footer.js
@@ -3,15 +3,52 @@ import Image from 'next/image'
 import Logo from '../../public/Logo.svg'
 import Logo_Camara from '../../public/Logo-CamaraDeputados.svg'
 
+import { IoLogoGithub, IoLogoLinkedin, IoLogoWhatsapp } from "react-icons/io5";
+
 import Style from './Style.module.css'
 
 const Footer = () => {
   return (
     <footer className={Style.Footer}>
-        <div className={Style.imagens}>
-            <Image src={Logo} alt="Logo_Saiba-Politico" className={Style.Image}/>
-            <Image src={Logo_Camara} alt="Logo_Saiba-Politico" className={Style.ImageCamara}/>
+      <section className={Style.Imagens}>
+        <Image src={Logo} alt="Logo_Saiba-Politico" className={Style.Image}/>
+        <Image src={Logo_Camara} alt="Logo_Saiba-Politico" className={Style.ImageCamara}/>
+      </section>
+
+      <section className={Style.Infos}>
+        <h1>Desenvolvedores</h1>
+        <div className={Style.NameInfos}>
+          <table className={Style.Table}>
+            <tr>
+              <td>Brenno Alves</td>
+              <td>21214290011</td>
+              <td className={Style.Td}><IoLogoGithub /><IoLogoLinkedin /><IoLogoWhatsapp /></td>
+            </tr>
+            <tr>
+              <td>Brenno Alves</td>
+              <td>21214290011</td>
+              <td className={Style.Td}><IoLogoGithub /><IoLogoLinkedin /><IoLogoWhatsapp /></td>
+            </tr>
+            <tr>
+              <td>Brenno Alves</td>
+              <td>21214290011</td>
+              <td className={Style.Td}><IoLogoGithub /><IoLogoLinkedin /><IoLogoWhatsapp /></td>
+            </tr>
+          </table>
+          <table className={Style.Table}>
+            <tr>
+              <td>Brenno Alves</td>
+              <td>21214290011</td>
+              <td className={Style.Td}><IoLogoGithub /><IoLogoLinkedin /><IoLogoWhatsapp /></td>
+            </tr>
+            <tr>
+              <td>Brenno Alves</td>
+              <td>21214290011</td>
+              <td className={Style.Td}><IoLogoGithub /><IoLogoLinkedin /><IoLogoWhatsapp /></td>
+            </tr>
+          </table>
         </div>
+      </section>
     </footer>
   )
 }
--- saiba-politico/pages/index.js
+++ saiba-politico/pages/index.js
@@ -8,14 +8,30 @@ export default function Home() {
   return (
     <>
     <style jsx global>{`
-            body {
+      @import url('https://fonts.googleapis.com/css2?family=Jura:wght@300;400;500;600;700&display=swap');
+
+            :root {
+              --White: #ffffff;
+              --DarkBlue: #0041B2;
+              --Green: #00FF15;
+
+              // SPACING
+
+              --addingGeneral: 15px 110px;
+            }
+
+            * {
               margin: 0;
               padding: 0;
+              font-family: 'Jura', sans-serif;
+              font-weight: 500;
+              font-size: 16px;
             }
     `}</style>
-    
+
       <Header />
       <Footer />
+      
     </>
   )
 }
